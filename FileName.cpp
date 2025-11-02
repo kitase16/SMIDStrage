4e574461b17ba8071932648ac2690d62a6f3418b
kitase16 <kitasebeef@gmail.com>
2025-11-03 02:48:53 +0900

    refactering.

--- FileName.cpp
+++ FileName.cpp
@@ -5,6 +5,8 @@
 //i want to soso speed scripting like so c++ futuer.
 //hello concern.
 
+// vc 2026 instder is crashed...?
+
 int F1() {
 	return 0x123;
 }
@@ -17,12 +19,8 @@ std::string F3() {
 	return "1234";
 }
 
-
-template<class... a>
-const auto& SIMDStrage(const a&... arg) {
-	return std::tuple<const a&...>{ arg... };// i hope auto correct.
-	//return []{ arg... };//bad bad.
-	//return { ...arg };//hope this.
+template<class... arg> const auto& SIMDStrage(const arg&... a) {
+	return std::tuple<const arg&...>({ a... });
 }
 
 int main() {
@@ -30,3 +28,19 @@ int main() {
 
 	std::cout << std::get<1>(A) << std::endl;
 }
+
+/** /
+//const auto&& SIMDStrage(const auto&&... arg){
+template<class... a> std::tuple<a&&...>SIMDStrage(const a&&... arg) {
+	return std::tuple<const a&&...>{ std::forward_as_tuple<a...>(arg...) };// i hope auto correct.
+	//return std::tuple<const auto&&...>{ arg... };// i hope auto correct.
+	//return []{ arg... };//bad bad.
+	//return { arg... };//hope this.
+}
+/**/
+/** /int main() {
+	//auto A = SIMDStrage(F1(), F2(), F3());
+
+	//std::cout << std::get<1>(A) << std::endl;
+}
+/**/
